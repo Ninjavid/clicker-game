79d678c4416347b4cb067c629a2cc080d51d0d5b
Ninjavid <dborja@s.tooeletech.edu>
2022-09-19 18:37:13 -0600

    delete button added and fixed alerts

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-const button = document.getElementById("clicker");
+const clicker = document.getElementById("clicker");
 let pointsCounter = document.getElementById("points");
-let points = 0
 let autoclicker = document.getElementById("autoclicker");
+let reset = document.getElementById("resetButton");
+let points = 0
+let clickerUpgrade = false;
 
-button.addEventListener("click", () => {
+clicker.addEventListener("click", () => {
     points += 1
     pointsCounter.innerHTML = points
 });
 
 function buyAutoclicker() {
-    if (points >= 10) {
-        points -= 10
-        alert("upgrade bought")
-        setTimeout(() => {
-            points += 1
-        }, 1000)
+        if (points >= 10 && !clickerUpgrade) {
+            points -= 10
+           alert("upgrade bought")
+           setTimeout(() => {
+               points++
+               clickerUpgrade = true;
+           }, 1000)
+        }
+    else if(points >= 10 && clickerUpgrade) {
+        alert("already bought")
     } else {
         alert("please get 10 points")
     }
 }
 
+function resetButton() {
+    if(points > 0){
+        points -= points;
+        pointsCounter.innerHTML = 0;
+    }
+}
+
 autoclicker.addEventListener("click", buyAutoclicker);
+reset.addEventListener("click", resetButton);
